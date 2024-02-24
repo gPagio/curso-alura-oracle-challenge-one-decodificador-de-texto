8bfcfab49c2ef21324f4f964c1707cf6c4511c9b
gPagio <guiguipagio@gmail.com>
2024-02-24 14:00:12 -0300

    feat: implementação inicial de reposividade
    Implementação foi tomada como base o iPhone 12 Pro. Futuras melhorias em relação a responsividade serão incluídas aos poucos.

--- scripts/script.js
+++ scripts/script.js
@@ -21,9 +21,15 @@ function descriptografar() {
 }
 
 function copiarParaAreaTransferencia() {
-    var texto = getTextoExibido();
-    console.log(texto);
-    navigator.clipboard.writeText(texto);
+    try {
+        var texto = getTextoExibido();
+        console.log(texto);
+        navigator.clipboard.writeText(texto);
+    } catch (error) {
+        console.log('Nenhum texto foi processado');
+        alert('Nenhum texto foi processado');
+        navigator.clipboard.writeText('');
+    }
 }
 
 function getTextoDigitado(){
@@ -31,13 +37,35 @@ function getTextoDigitado(){
 }
 
 function getTextoExibido(){
-    return document.querySelector('.painel__direito__interagivel__texto').textContent.trim();
+    return document.querySelector('.painel__direito__interagivel__processado__texto').textContent.trim();
 }
 
 function exibirNaTela(mensagem){
     if (!mensagem || mensagem.length === 0) {
-        document.querySelector('.painel__direito__interagivel__texto').textContent = "Nenhum texto foi digitado";
+        document.querySelector('.painel__direito').innerHTML = exibirPainelDireitoInicial();
     } else {
-        document.querySelector('.painel__direito__interagivel__texto').textContent = mensagem;
+        document.querySelector('.painel__direito').innerHTML = exibirPainelDireitoTextoProcessado(mensagem);
     }
+}
+
+function exibirPainelDireitoInicial(){
+    return `<div class="painel__direito__interagivel">
+                <img class="painel__direito__interagivel__imagem" src="assets/High quality products 1 1.svg" alt="Imagem Nada Encontrado">
+                <div class="painel__direito__interagivel__texto">
+                    <div class="painel__direito__interagivel__texto__principal">
+                        Nenhuma mensagem encontrada
+                    </div>
+                    <div class="painel__direito__interagivel__texto__secundario">
+                        Digite um texto que você deseja criptografar ou descriptografar.
+                    </div>
+                </div>
+            </div>`;
+}
+
+function exibirPainelDireitoTextoProcessado(mensagem){
+    return `<div class="painel__direito__interagivel__processado">
+                <div class="painel__direito__interagivel__processado__texto">
+                    ${mensagem}
+                </div>
+            </div>`;
 }
